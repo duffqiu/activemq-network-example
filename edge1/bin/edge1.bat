6d0c65b8d0df9e5708cc7722570a2db03835b520
Duff Qiu <duffqiu@gmail.com>
2015-06-04 15:05:15 +0800

    init

---
+++ edge1/bin/edge1.bat
@@ -0,0 +1,13 @@
+@echo off
+set ACTIVEMQ_HOME="/home/duff/activemq/activemq"
+set ACTIVEMQ_BASE="/home/duff/activemq/edge1"
+
+set PARAM=%1
+:getParam
+shift
+if "%1"=="" goto end
+set PARAM=%PARAM% %1
+goto getParam
+:end
+
+%ACTIVEMQ_HOME%/bin/activemq %PARAM%
